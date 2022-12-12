7a7f9120324c023ee900a8f1cf0f769cfa39b8f1
Shohan20lac <r.shohan12@gmail.com>
2022-12-13 02:43:43 +0600

    1 done

--- app/src/main/java/com/example/portrait/SliderAdapter.java
+++ app/src/main/java/com/example/portrait/SliderAdapter.java
@@ -5,6 +5,7 @@ import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -25,19 +26,22 @@ public class SliderAdapter extends PagerAdapter {
 
     // Arrays
 
-    public String[] slide_headings = {
+    public String[] slide_heading = {
+            "Welcome to my virtual resume!",
             "I AM A RECRUITER!",
             "I AM A RESEARCHER!!",
             "I AM A HUMAN!"
     };
 
     public int[] slide_images = {
+            R.drawable.shohan,
             R.drawable.man,
             R.drawable.woman_blue,
             R.drawable.woman_red
     };
 
     public String[] slide_descs = {
+            "Swipe left until you find a label that best describes you.",
             "I am only interested in:",
             "I want to learn more about:",
             "I am curious about:"
@@ -45,7 +49,7 @@ public class SliderAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return slide_headings.length;
+        return slide_heading.length;
     }
 
     @Override
@@ -61,9 +65,24 @@ public class SliderAdapter extends PagerAdapter {
         ImageView slideImageView = (ImageView) view.findViewById(R.id.slide_image);
         TextView slideHeading = (TextView) view.findViewById(R.id.slide_heading);
         TextView slideDescription = (TextView) view.findViewById(R.id.slide_desc);
+        CheckBox checkbox = (CheckBox) view.findViewById(R.id.checkBox);
 
         slideImageView.setImageResource(slide_images[position]);
-        slideHeading.setText(slide_descs[position]);
+
+        slideHeading.setText(slide_heading[position]);
+
+        slideDescription.setText(slide_descs[position]);
+
+        if(position==0){
+            checkbox.setVisibility(View.GONE);
+
+//            ViewGroup.LayoutParams heading_params=slideHeading.getLayoutParams();
+//            heading_params.height=267;
+//            slideHeading.setLayoutParams(heading_params);
+        }
+        else{
+            checkbox.setText("Text");
+        }
 
         container.addView(view);
 
